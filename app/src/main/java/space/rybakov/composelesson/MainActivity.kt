7ede53384f408006b0871ebec406832069dacad9
Dmitriy <dimryb@bk.ru>
2022-03-27 21:54:41 +0700

    Контейнеры Row и Collumn

--- app/src/main/java/space/rybakov/composelesson/MainActivity.kt
+++ app/src/main/java/space/rybakov/composelesson/MainActivity.kt
@@ -3,24 +3,55 @@ package space.rybakov.composelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import space.rybakov.composelesson.ui.theme.ComposeLessonTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            newText()
+            Row(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5f)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .fillMaxHeight()
+                        .fillMaxWidth(0.5f),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "Hello World!")
+                    Text(text = "Girona")
+                    Text(text = "Madrid")
+                }
+                Column(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .fillMaxHeight()
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "Hello World!")
+                    Text(text = "Girona")
+                    Text(text = "Madrid")
+                }
+            }
+
         }
     }
-}
-
-@Preview (showBackground = true)
-@Composable
-fun newText(){
-    Text(text = "Hello World!")
 }
