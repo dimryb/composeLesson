849f9149a65e985020d2bc1a1c53514b735490cd
Dmitriy <dimryb@bk.ru>
2022-03-28 22:03:02 +0700

    Использование Card, Box, Image

--- app/src/main/java/space/rybakov/composelesson/MainActivity.kt
+++ app/src/main/java/space/rybakov/composelesson/MainActivity.kt
@@ -3,16 +3,24 @@ package space.rybakov.composelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import space.rybakov.composelesson.ui.theme.ComposeLessonTheme
 
 
@@ -20,38 +28,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.5f)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .background(Color.Red)
-                        .fillMaxHeight()
-                        .fillMaxWidth(0.5f),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Text(text = "Hello World!")
-                    Text(text = "Girona")
-                    Text(text = "Madrid")
-                }
-                Column(
-                    modifier = Modifier
-                        .background(Color.Green)
-                        .fillMaxHeight()
-                        .fillMaxWidth(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Text(text = "Hello World!")
-                    Text(text = "Girona")
-                    Text(text = "Madrid")
+            Column() {
+                for (i in  1..7) {
+                    listItem("Zac Efron", "Actor $i")
                 }
             }
+        }
+    }
+}
 
+@Composable
+private fun listItem(name: String, prof: String){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp, vertical = 6.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ){
+        Box( ){
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(start = 16.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
         }
     }
 }
