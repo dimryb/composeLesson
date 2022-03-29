52df84fc3244d14b4fd343150a8e0520464a7a4d
Dmitriy <dimryb@bk.ru>
2022-03-30 00:18:53 +0700

    Прием действий с элементов

--- app/src/main/java/space/rybakov/composelesson/MainActivity.kt
+++ app/src/main/java/space/rybakov/composelesson/MainActivity.kt
@@ -1,10 +1,14 @@
 package space.rybakov.composelesson
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,8 +33,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
-                for (i in  1..7) {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+            ) {
+                for (i in  1..10) {
                     listItem("Zac Efron", "Actor $i")
                 }
             }
@@ -42,7 +50,18 @@ private fun listItem(name: String, prof: String){
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 10.dp, vertical = 6.dp),
+            .padding(horizontal = 10.dp, vertical = 6.dp)
+            .pointerInput(Unit){
+                detectHorizontalDragGestures { change, dragAmount ->
+                    Log.d("MyLog", "Drag $dragAmount")
+                }
+                detectTapGestures{ // Не как не работает
+                    Log.d("MyLog", "TapGestures")
+                }
+            }
+            .clickable {
+                Log.d("MyLog", "Clicked")
+            },
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
     ){
