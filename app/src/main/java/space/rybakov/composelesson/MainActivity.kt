ec3f9f14881f8e3717ae9c6bd54abdefa0f32db3
Dmitriy <dimryb@bk.ru>
2022-04-02 19:13:23 +0700

    LazyColumn

--- app/src/main/java/space/rybakov/composelesson/MainActivity.kt
+++ app/src/main/java/space/rybakov/composelesson/MainActivity.kt
@@ -1,78 +1,43 @@
 package space.rybakov.composelesson
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import space.rybakov.composelesson.ui.theme.ComposeLessonTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .fillMaxSize(),
-                contentAlignment = Alignment.Center) {
-                circleItem()
+                    .fillMaxSize()
+            ) {
+                itemsIndexed(
+                    listOf("item1", "item2", "end", "finish")
+                ){ _, item ->
+                    Text(
+                        text = item,
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(vertical = 10.dp)
+                    )
+                }
             }
         }
     }
 }
 
-@Composable
-private fun circleItem(){
-    val counter = remember {
-        mutableStateOf(0)
-    }
-    val color = remember {
-        mutableStateOf(Color.Blue)
-    }
-    Box(modifier = Modifier
-        .size(100.dp)
-        .background(
-            color = color.value,
-            shape = CircleShape)
-        .clickable {
-            when(++counter.value){
-                10 -> color.value = Color.Red
-                20 -> color.value = Color.Green
-            }
-        },
-        contentAlignment = Alignment.Center
-    ){
-        Text(
-            text = "${counter.value}",
-            style = TextStyle(
-                color = Color.White,
-                fontSize = 20.sp))
-    }
-}
